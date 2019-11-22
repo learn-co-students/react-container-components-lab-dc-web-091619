2c1609fc9f1a74c4d23c914c86870425208bad86
github username <github email address>
2019-11-22 16:37:10 -0500

    done

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -5,7 +5,7 @@ import Review from './Review'
 const MovieReviews = (props) => {
   return(
     <div className="review-list">
-      {props.reviews.length !== 0 ? props.reviews.map(rev => <Review key={rev.display_title} review={rev} />) : null}
+      {props.reviews.map(rev => <Review key={rev.display_title} review={rev} />)}
     </div>
   )
 }
